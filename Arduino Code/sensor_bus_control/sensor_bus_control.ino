ae058d62edccbf09c53ac1c4d514d65aaa4fe9d5
Zack <zackmj99@gmail.com>
2022-05-23 13:43:16 -0500

    Figured out why many sensors were not working!

--- Arduino Code/sensor_bus_control/sensor_bus_control.ino
+++ Arduino Code/sensor_bus_control/sensor_bus_control.ino
@@ -1,7 +1,6 @@
 #include <EEPROM.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <SerialTransfer.h>
 
 #define NUM_SENSORS 81
 #define ONE_WIRE_BUS 2 //set pin number used
@@ -12,52 +11,12 @@ DallasTemperature sensors(&oneWire);
 
 double measurement_rate = 1; //Time between measurements in seconds
 DeviceAddress address_list[NUM_SENSORS];
+DeviceAddress currentAddr;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   sensors.begin();
-  DeviceAddress tempAdr;
-  for(int i = 0; i < NUM_SENSORS; i++){
-    EEPROM.get(8*i, tempAdr);
-    if(sensors.validAddress(tempAdr)){
-      memcpy(address_list[i], tempAdr, sizeof(DeviceAddress));
-      Serial.print("Detected Sensor at Position: ");
-      Serial.print(i);
-      Serial.print(", Address: ");
-      printAddress(tempAdr);
-      Serial.println("|");
-    }
-    else{
-      Serial.print("Failed to Detect Sensor at Position: ");
-      Serial.print(i);
-      Serial.print(", Address: ");
-      printAddress(tempAdr);
-      Serial.println("|");
-    }
-  }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  sensors.requestTemperatures();
-  
-  for (int i = 0; i < 9; i++){
-    for (int j = 0; j < 9; j++){
-      int location = (i*9)+j;
-      float temperature = sensors.getTempC(address_list[0]);
-      String packet = String("&");
-      packet += i;
-      packet += ",";
-      packet += j;
-      packet += ",";
-      packet += temperature;
-      packet += "|";
-      Serial.println(packet);
-    }
-  }
-  Serial.println("End of timestep|");
-  delay(1000 * measurement_rate);
 }
 
 // function to print a device address
@@ -69,4 +28,31 @@ void printAddress(DeviceAddress deviceAddress)
     if (deviceAddress[i] < 16) Serial.print("0");
     Serial.print(deviceAddress[i], HEX);
   }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensors.requestTemperatures();
+  int EAddress = 0;
+  for (int n = 0; n < NUM_SENSORS; n++){
+      for(byte b = 0; b < 8; b++){
+          currentAddr[b] = EEPROM.read(EAddress);
+          EAddress += 1;
+      }
+      int i = n / 9;
+      int j = n % 9;
+      printAddress(currentAddr);
+      float temperature = sensors.getTempC(currentAddr);
+      Serial.println(temperature);
+      String packet = String("&");
+      packet += i;
+      packet += ",";
+      packet += j;
+      packet += ",";
+      packet += temperature;
+      packet += "|";
+      Serial.println(packet);
+    }
+  Serial.println("End of timestep|");
+  delay(1000 * measurement_rate);
 }
